bf3d950d23b357c168aab544d26edddf180ccc21
nh-g <giangngohong@gmail.com>
2021-10-24 16:37:35 +0200

    set up layout styles

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,15 +1,22 @@
+// NPM Packages
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+// Project files
 import Home from '../Home'
+import GlobalStyles from 'components/GlobalStyles'
 
 export default function App() {
 	return (
-		<BrowserRouter>
-			<Switch>
-				<Route path="/">
-					<Home />
-				</Route>
-			</Switch>
-		</BrowserRouter>
+		<>
+			<GlobalStyles />
+			<BrowserRouter>
+				<Switch>
+					<Route path="/">
+						<Home />
+					</Route>
+				</Switch>
+			</BrowserRouter>
+		</>
 	)
 }
